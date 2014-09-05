b63e76356bf2a773ba917429999a3a24053af362
taufekj <taufek.johar@my.experian.com>
2014-09-05 19:59:31 +0800

    added angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name segoraClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the segoraClientApp
+ */
+angular.module('segoraClientApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name segoraClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the segoraClientApp
+ */
+angular.module('segoraClientApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
