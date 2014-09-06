ee03790c0398c7a9dfc3b081b7e87f20f625c0b2
dhydrated <dhydrated@gmail.com>
2014-09-06 18:33:10 +0800

    added morgan

--- web.js
+++ web.js
@@ -1,7 +1,11 @@
 var gzippo = require('gzippo');
 var express = require('express');
+var morgan = require('morgan')
 var app = express();
  
-app.use(express.logger('dev'));
+//app.use(express.logger('dev'));
+morgan('combined', {
+  skip: function (req, res) { return res.statusCode < 400 }
+})
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
