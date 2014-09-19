7f2f7a2f78af1fab75483af4ba0744d64848a728
taufekj <taufek.johar@my.experian.com>
2014-09-19 08:26:47 +0800

    fixed the progress bar

--- app/scripts/services/status-service.js
+++ app/scripts/services/status-service.js
@@ -1,27 +1,31 @@
 'use strict';
 
 angular.module('segoraClientApp')
-    .service('StatusService', function() {
+    .service('StatusService', function($rootScope) {
         return {
             default: function() {           
                 $('#progressBarModal').modal('hide');
                 $('body').removeClass('modal-open');
                 $('.modal-backdrop').remove();
+                $rootScope.progressActive = "progress-success";
             },
             start: function() {
                 $('body').addClass('modal-open');
                 $('#progressBarModal').modal('show');
+                $rootScope.progressActive = "progress-striped active progress-warning";
             },
             stop: function(message) {
                 $('#progressBarModal').modal('hide');
                 $('body').removeClass('modal-open');
                 $('.modal-backdrop').remove();
+                $rootScope.progressActive = "progress-success";
                 
             },
             error: function(message) {
                 $('#progressBarModal').modal('hide');
                 $('body').removeClass('modal-open');
                 $('.modal-backdrop').remove();
+                $rootScope.progressActive = '';
             }
         }
     });
