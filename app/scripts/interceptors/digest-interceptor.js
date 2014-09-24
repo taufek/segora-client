8f7f144222c05c1baa7aabac62b35a8500cef4ab
dhydrated <dhydrated@gmail.com>
2014-09-24 23:47:12 +0800

    refactor digest

--- app/scripts/interceptors/digest-interceptor.js
+++ app/scripts/interceptors/digest-interceptor.js
@@ -3,33 +3,7 @@
 angular.module("segoraClientApp")
 .config(function($httpProvider, $provide) {
 
-        // var digestInterceptor = 
-        //     function($location, $q) {
-
-        //         var success = function(response) {
-        //             return response;
-        //         };
-
-        //         var error = function(response) {
-
-        //             if (response.status === 401 && response.config.url.indexOf(Settings.backendHost) != -1) {
-        //                 console.log('digest error');
-        //                 console.log(response);
-        //                 console.log($httpProvider);
-
-        //             }
-        //             return $q.reject(response);
-        //         };
-
-        //         return function(promise) {
-        //             return promise.then(success, error);
-        //         };
-        //     };
-
-        // $httpProvider.responseInterceptors.push(digestInterceptor);
-
         // register the interceptor as a service
-        //$q, $injector, md5, DigestHttp
         $provide.factory('digestInterceptor', 
             function($q, $injector, md5, DigestHttp, Settings, UserSessionService) {
             return {
@@ -37,23 +11,11 @@ angular.module("segoraClientApp")
                 'request': function(config) {
                     // do something on success
                     // console.log(config);
-
-
-                    // if (config.method == 'POST' || config.method == 'PUT') {
-
-                    // if (config.url.indexOf(Settings.backendHost) != -1) {
-                    //     console.log(config);
-                    // }
                     return config;
                 },
 
                 // optional method
-                'requestError': function(rejection) {
-                    // do something on error
-                    //          if (canRecover(rejection)) {
-                    //            return responseOrNewPromise
-                    //          }
-                    // console.log(rejection);
+                'requestError': function(rejection) {                 
 
                     return $q.reject(rejection);
                 },
@@ -68,22 +30,8 @@ angular.module("segoraClientApp")
                 // optional method
                 'responseError': function(rejection) {
                     // do something on error
-                    //          if (canRecover(rejection)) {
-                    //            return responseOrNewPromise
-                    //          }
-                    // console.log(rejection);
-                    // console.log(rejection.headers('WWW-Authenticate'));
-                    // console.log(rejection.config.headers.Authorization);
-
-
+                
                     if(!rejection.config.headers.Authorization){
-
-                        // window.counter++;
-
-                        // console.log(window.counter);     
-                        // console.log('resending request with authorization');
-
-                        // console.log(rejection);
 
                         var path = rejection.config.url.substring(Settings.backendHost.length, rejection.config.url.length);
                         var dh = new DigestHttp($injector.get('$http'), md5);
@@ -95,22 +43,13 @@ angular.module("segoraClientApp")
                             rejection.config.data,                                 
                             rejection.headers
                         );
-                        // console.log(promise);
-                        return promise;
                         
-
+                        return promise;          
                     }
                     else{
                         return $q.reject(rejection);
                     }
-                    // console.log($injector.get('$http'));
-                    // console.log(rejection.config.url.indexOf(Settings.backendHost));
-                    // console.log(Settings.backendHost.length);
-                    // console.log(rejection.config.url.substring(Settings.backendHost.length, rejection.config.url.length));
-
-                   
-
-                    // return $q.reject(rejection);
+                    
                 }
             };
         });
