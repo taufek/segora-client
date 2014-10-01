60ad93187855a5bcd81a7e459748dc77316ba8de
taufekj <taufek.johar@my.experian.com>
2014-10-01 10:26:54 +0800

    added delete payment confirmation

---
+++ app/scripts/directives/ng-confirm-click.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module("segoraClientApp")
+.directive('ngConfirmClick', function() {
+        return {
+            link: function(scope, element, attr) {
+                var msg = attr.ngConfirmClick || "Are you sure?";
+                var clickAction = attr.confirmedClick;
+                element.bind('click', function(event) {
+                    if (window.confirm(msg)) {
+                        scope.$eval(clickAction)
+                    }
+                });
+            }
+        };
+    }
+);
