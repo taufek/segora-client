64284c97cb702e9602db6bc1ab13be63b8c4227e
taufekj <taufek.johar@my.experian.com>
2014-10-01 18:36:47 +0800

    added address link

--- app/scripts/controllers/addresses-list.js
+++ app/scripts/controllers/addresses-list.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('segoraClientApp')
-    .controller('AddressesListCtrl', function($scope, data) {
+    .controller('AddressesListCtrl', function($scope, $location, UserService, data) {
         
 
         $scope.addresses = data.addresses;
 
-
+        
     });
