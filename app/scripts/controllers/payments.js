eaaa71cc7d9be102892996b682a51f1dbee75e53
taufekj <taufek.johar@my.experian.com>
2014-10-10 18:24:19 +0800

    updated search

--- app/scripts/controllers/payments.js
+++ app/scripts/controllers/payments.js
@@ -29,5 +29,20 @@ angular.module('segoraClientApp')
 
     $('.datepicker').datepicker({format:'dd-mm-yyyy'});
 
+    $scope.months = [
+    {'code':'1', 'name':'January'},
+    {'code':'2', 'name':'February'},
+    {'code':'3', 'name':'March'},
+    {'code':'4', 'name':'April'},
+    {'code':'5', 'name':'May'},
+    {'code':'6', 'name':'June'},
+    {'code':'7', 'name':'July'},
+    {'code':'8', 'name':'August'},
+    {'code':'9', 'name':'September'},
+    {'code':'10', 'name':'October'},
+    {'code':'11', 'name':'November'},
+    {'code':'12', 'name':'December'},
+    ];
+
 
   });
