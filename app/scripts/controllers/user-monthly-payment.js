99b353b0cde84068b22095dc028f002efe04df16
taufekj <taufek.johar@my.experian.com>
2015-01-04 00:57:40 +0800

    fixed print

--- app/scripts/controllers/user-monthly-payment.js
+++ app/scripts/controllers/user-monthly-payment.js
@@ -115,7 +115,7 @@ angular.module('segoraClientApp')
 
     $scope.getReceiptLink = function(paymentId){
 
-      return Settings.backendHost + "/pdf_generator?url=" + Settings.backendHost + "/payment_receipt/" + paymentId;
+      return Settings.backendHost + "/pdf_generator?url=" + (Settings.backendHost).replace('https','http') + "/payment_receipt/" + paymentId;
     }
 
     $scope.editBankReference = function(month){
