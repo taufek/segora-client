69d2bd65c92ae8272e05d81db2c0b355af0376c0
taufekj <taufek.johar@my.experian.com>
2015-01-05 23:02:32 +0800

    fixed the main controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -19,7 +19,7 @@ angular.module('segoraClientApp')
         	return UserSessionService.hasSession();
         }
 
-        if($scope.hasSession){
+        if($scope.hasSession()){
             var currentYear = new Date().getFullYear();
             $location.path('/user/'+UserSessionService.getUser()._id+'/monthly_payment/'+currentYear); 
         }
