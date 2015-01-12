e5179fec7b6ca109e165405f72f19f9efa79bc87
taufekj <taufek.johar@my.experian.com>
2015-01-12 15:10:51 +0800

    updated an build

--- app/scripts/services/bank-service.js
+++ app/scripts/services/bank-service.js
@@ -7,7 +7,20 @@ angular.module('segoraClientApp')
             {code: "affin", name:"Affin Bank"},
             {code: "alliance", name:"Alliance Bank"},
             {code: "am", name:"AmBank"},
-            {code: "alrajhi", name:"Al Rajhi Bank"}
+            {code: "alrajhi", name:"Al Rajhi Bank"},
+            {code: "islam", name:"Bank Islam"},
+            {code: "muamalat", name:"Bank Muamalat"},
+            {code: "cimb", name:"CIMB Bank"},
+            {code: "citi", name:"Citibank"},
+            {code: "kfh", name:"Kuwait Finance House"},
+            {code: "hsbc", name:"HSBC Bank"},
+            {code: "hongleong", name:"Hong Leong Bank"},
+            {code: "maybank", name:"Maybank"},
+            {code: "ocbc", name:"OCBC Bank"},
+            {code: "public", name:"Public Bank"},
+            {code: "rhb", name:"RHB Bank"},
+            {code: "standchart", name:"Standard Chartered"},
+            {code: "others", name:"Others"}
         ];
         // Public API here
         return {
