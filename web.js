e8185d2f6fbecec2be4fb8aa8d9c88eec15eda57
taufekj <taufek.johar@my.experian.com>
2015-01-13 18:10:38 +0800

    updated the cache for heroku

--- web.js
+++ web.js
@@ -3,6 +3,13 @@ var express = require('express');
 var morgan = require('morgan')
 var app = express();
  
+app.configure(function () {
+  app.use(function (req, res, next) {
+    res.setHeader('Cache-Control', 'max-age=86400');
+    return next();
+  });
+});
+
 //app.use(express.logger('dev'));
 morgan('combined', {
   skip: function (req, res) { return res.statusCode < 400 }
