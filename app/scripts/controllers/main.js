8484b3c8324ba8b331ab5230e382a9c3abe17f0d
taufekj <taufek.johar@my.experian.com>
2015-01-15 17:37:29 +0800

    remind user to change their default password upon login

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the segoraClientApp
  */
 angular.module('segoraClientApp')
-    .controller('MainCtrl', function($scope, $location, UserSessionService) {
+    .controller('MainCtrl', function($scope, $location, UserSessionService, CredentialService, FlashService) {
         $scope.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
@@ -23,8 +23,16 @@ angular.module('segoraClientApp')
 
             var hash = UserSessionService.getHash();
 
-            
-            var currentYear = new Date().getFullYear();
-            $location.path('/user/'+UserSessionService.getUser()._id+'/monthly_payment/'+currentYear); 
+            if (hash == '10073a2472e3d3e7f26a8a37573fb678'){
+
+                CredentialService.getByUserId(UserSessionService.getUser()._id, function(credential){
+                    FlashService.setMessage('Please change your default password.', 'danger');
+                    $location.path('/user/'+UserSessionService.getUser()._id+'/credential/'+credential._id);                 
+                });
+            }
+            else{
+                var currentYear = new Date().getFullYear();
+                $location.path('/user/'+UserSessionService.getUser()._id+'/monthly_payment/'+currentYear);                 
+            }
         }
     });
