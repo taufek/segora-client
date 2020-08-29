92c40b0896b3218878c5ec6efcbbb2f5c0e85b3e
Taufek Johar <taufek@gmail.com>
2020-08-29 18:42:58 +0800

    backendHost from env

--- app/scripts/settings.js
+++ app/scripts/settings.js
@@ -3,7 +3,7 @@
 angular.module("segoraClientApp")
 .constant('Settings', {
     appName: "SEGORA",
-    backendHost: "https://segora-services.herokuapp.com",
-    // backendHost: "http://localhost:3000",	
+    backendHost: process.env.TIMES || "https://segora-services.herokuapp.com",
+    // backendHost: "http://localhost:3000",
     rowCount: 10
 });
